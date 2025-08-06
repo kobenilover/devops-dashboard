856566b18547ada19f1ea53a2f1e99ab97754c8c
Alan Kadzhaev <alan11kadzaev@gmail.com>
2025-08-06 16:41:13 +0300

    Initial commit

---
+++ script.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+if [ ! -f ~/devops-dashboard/docs/index.html ]; then
+	if [ ! -d ~/devops-dashboard/docs ]; then
+		mkdir ~/devops-dashboard/docs
+	fi
+	touch ~/devops-dashboard/docs/index.html
+fi
+
+echo "<h1>Devops Dashboard</h1>" > ~/devops-dashboard/docs/index.html
+
+add_to_file() {
+	echo "<p>$1</p>" >> ~/devops-dashboard/docs/index.html
+}
+
+host=$(hostname)
+add_to_file "Host: $host"
+
+day=$(date +%Y-%m-%d)
+hours=$(date +%H:%M:%S)
+time_now="${day} ${hours}"
+add_to_file "Time: $time_now"
+
+cpu=`top -bn1 | awk '/Cpu\(s\):/ {printf "%.1f%%\n", $2 + $4}'`
+add_to_file "CPU: $cpu"
+
+total=$(free -m | awk '/^Память:/ {print $2}')
+used=$(free -m | awk '/^Память:/ {print $3}')
+percent=$((used * 100 / total))
+add_to_file "Memory: $percent% used"
+
+diskp=$(df / -h | awk 'NR == 2 {print $5}')
+add_to_file "Disk: $diskp used"
